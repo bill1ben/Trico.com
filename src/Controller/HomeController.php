f6ed9a76b74d27739f57711284e6046a61b1475c
bill1ben <fcbilel22@gmail.com>
2020-09-10 07:34:06 +0200

    premier commit, instalation et page accueil

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+     * @Route("/hello/{prenom}", name="name")
+     */
+    public function Hello($prenom = 'bill')
+    {
+        return new Response("salut $prenom");
+    }
+}
